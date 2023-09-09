7ce07a740e2867c5d81f6ceb8f0a675c84dd02a2
Fernando Silva <fernandoadmti@hotmail.com>
2023-09-09 17:26:38 -0300

    criando as models, conexao com o banco e reestruturando a organizacao de alguns arquivos

--- src/main/java/med/voll/api/model/dto/DadosEndereco.java
+++ src/main/java/med/voll/api/model/dto/DadosEndereco.java
@@ -1,4 +1,4 @@
-package med.voll.api.model;
+package med.voll.api.model.dto;
 
 public record DadosEndereco(String logradouro, String bairro, String cep, String cidade, String uf,
                             String complemento, String numero) {
